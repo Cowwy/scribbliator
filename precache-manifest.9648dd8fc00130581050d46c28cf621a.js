bad3e1e790835d2e8d6aad9aa1c62117324e44eb
Frankie Law <frankie.sc.law@hotmail.ca>
2019-12-26 14:12:01 -0500

    Updates

---
+++ precache-manifest.9648dd8fc00130581050d46c28cf621a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "664516f1b900a0db185bab6aed6f2fa3",
+    "url": "/scribbliator/index.html"
+  },
+  {
+    "revision": "39860d2700e925b1d987",
+    "url": "/scribbliator/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "4ab847921bb9211ef61d",
+    "url": "/scribbliator/static/css/main.84f137e0.chunk.css"
+  },
+  {
+    "revision": "39860d2700e925b1d987",
+    "url": "/scribbliator/static/js/2.fc7e389a.chunk.js"
+  },
+  {
+    "revision": "dcc74a8cd978ffaa0e12c4869e3af818",
+    "url": "/scribbliator/static/js/2.fc7e389a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ab847921bb9211ef61d",
+    "url": "/scribbliator/static/js/main.4bf909e1.chunk.js"
+  },
+  {
+    "revision": "d36db2eb9d8d371e6ac2",
+    "url": "/scribbliator/static/js/runtime-main.df91561f.js"
+  },
+  {
+    "revision": "662ecba529e89681e77f6a049b12b724",
+    "url": "/scribbliator/static/media/scribbliator-home-3.662ecba5.jpg"
+  }
+]);
