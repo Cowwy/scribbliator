81b0a0163e82891020666c936d5d0a6a46cb4d67
Frankie Law <frankie.sc.law@hotmail.ca>
2020-01-09 15:04:15 -0500

    Updates

--- precache-manifest.2647562400638ef76b83af96104968bc.js
+++ precache-manifest.2647562400638ef76b83af96104968bc.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dc3054c841e5d84d64977c256249022d",
+    "revision": "6e45bf7bc7f1a1d8cad95ccb864b71fa",
     "url": "/scribbliator/index.html"
   },
   {
-    "revision": "66954c786e81707c6be8",
+    "revision": "c1fdd2ebd7ecf3825fde",
     "url": "/scribbliator/static/css/2.64a99b10.chunk.css"
   },
   {
-    "revision": "ed993f0fd71411316492",
+    "revision": "e2de0e0962768cda054a",
     "url": "/scribbliator/static/css/main.6106cb83.chunk.css"
   },
   {
-    "revision": "66954c786e81707c6be8",
-    "url": "/scribbliator/static/js/2.606f2917.chunk.js"
+    "revision": "c1fdd2ebd7ecf3825fde",
+    "url": "/scribbliator/static/js/2.84a29f5c.chunk.js"
   },
   {
     "revision": "c2e558a0b8465286ae0f1ce2d53eec13",
-    "url": "/scribbliator/static/js/2.606f2917.chunk.js.LICENSE"
+    "url": "/scribbliator/static/js/2.84a29f5c.chunk.js.LICENSE"
   },
   {
-    "revision": "ed993f0fd71411316492",
-    "url": "/scribbliator/static/js/main.4acab9ab.chunk.js"
+    "revision": "e2de0e0962768cda054a",
+    "url": "/scribbliator/static/js/main.48240ecc.chunk.js"
   },
   {
     "revision": "d36db2eb9d8d371e6ac2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scribbliator/precache-manifest.3383b70b5439249ceb89895b2d818b79.js"
+  "/scribbliator/precache-manifest.2647562400638ef76b83af96104968bc.js"
 );
 
 self.addEventListener('message', (event) => {
